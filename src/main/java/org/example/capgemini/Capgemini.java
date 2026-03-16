bae7a5e14c381db0f869b524419fd9d7fc89eb67
sagar vitkare <svitkare07@gmail.com>
2026-03-16 17:22:48 +0530

    Capgemini 2nd round question

--- src/main/java/org/example/capgemini/Capgemini.java
+++ src/main/java/org/example/capgemini/Capgemini.java
@@ -1,12 +1,37 @@
 package org.example.capgemini;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Capgemini {
+
+    public void secondRoundQuestion(){
+//        Find the first non-repeated character in a string.
+//        String input = "aabbcddeff";
+
+
+        String str = "aabbcddeff";
+
+        Map<Character,Long> charCount = str.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(
+                        Function.identity(),
+                        LinkedHashMap::new,
+                        Collectors.counting()
+                ));
+
+        charCount.entrySet().stream()
+                .filter(entry -> entry.getValue()==1)
+                .map(Map.Entry::getKey).findFirst();
+
+    }
     public static void main(String[] args) {
+
+        //First round
         HashMap<Integer,String> hashMap = new HashMap<>();
 
         hashMap.put(1,"Sagar");
